69c6ab15eb2356c6d89d0bb94d807e16399fdd88
yamuna1996d <yamunag19@gmail.com>
2020-04-16 20:14:36 +0530

    node admin route

--- admin.js
+++ admin.js
@@ -7,7 +7,7 @@ var adminrouter = require('./routes/adminrout');
 app.use(parser.urlencoded({extended:false}));
 app.use('/admin',adminrouter);
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://dbuser:ava1996@cluster0-b6xbb.mongodb.net/test?retryWrites=true&w=majority");
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Server Started");
 });
